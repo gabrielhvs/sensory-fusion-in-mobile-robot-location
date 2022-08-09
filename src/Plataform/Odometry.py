ea7bf9c8264189f8c56628f37f1b2be6b429b91c
Breno Pinheiro de Meneses <breno.meneses@ee.ufcg.edu.br>
2022-08-09 14:02:23 -0300

    Atualização do código da odometria

--- src/Plataform/Odometry.py
+++ src/Plataform/Odometry.py
@@ -1,12 +1,11 @@
 from cmath import pi
-import scipy
 from numpy import *
 import numpy as np
 import matplotlib.pyplot as plt
 #!/usr/bin/env python
 import rospy
 import time
-from math import pi,cos
+from math import pi,cos, sin,tan, atan
 from std_msgs.msg import Int16
 from nav_msgs.msg import Odometry
 
@@ -23,13 +22,12 @@ class Calc_Odom:
     
     def initInstances(self):
         self.odom_msg = Odometry()
-        self.beforeR = rospy.get_rostime()
-        self.beforeL = rospy.get_rostime()
+        self.dt = 1/1000
         self.rate = rospy.Rate(10)
     
     def initSubscribers(self):
-        self.velR = self.rospy.Subscriber('velRight', Int16, self.get_velRight, queue_size=10)
-        self.velL = self.rospy.Subscriber('velLeft', Int16, self.get_velLeft, queue_size=10)
+        self.velR = self.rospy.Subscriber('rpm_wheel_right', Int16, self.get_velRight, queue_size=10)
+        self.velL = self.rospy.Subscriber('rpm_wheel_left', Int16, self.get_velLeft, queue_size=10)
         self.rate = rospy.Rate(10)
         
     def initPublishers(self):
@@ -39,41 +37,33 @@ class Calc_Odom:
     def initvariables(self):
         self.R = (11.16/2)/100 #in meters
         self.L = (2*2.13+13.84+13.35)/100 #in meters
-        self.oldTheta = 0
-        self.oldX = 0
-        self.oldY = 0
-        self.newTheta = 0
-        self.newX = 0
-        self.newY = 0
+        self.Theta = 0
+        self.X = 0
+        self.Y = 0
         
     def get_velRight(self, msg):
 
         self.velRight = (msg.data/60)*2*pi*self.R
-        self.nowR = rospy.get_rostime().secs
-
+        #self.velRight = msg.data
+        
     def get_velLeft(self, msg):
 
         self.velLeft = (msg.data/60)*2*pi*self.R
-        self.nowL = rospy.get_rostime().secs
+        #self.velLeft = msg.data
     
     def get_distR(self):
-
-        dt = (self.nowR-self.beforeR)
-        self.distanceR = self.velRight*dt
+        self.distanceR = self.velRight*self.dt
         return self.distanceR
 
     def get_distL(self):
-
-        dt = (self.nowL-self.beforeL)
-        self.distanceL = self.velLeft*dt
+        self.distanceL = self.velLeft*self.dt
         return self.distanceL
     
-    def get_quaternion_from_euler(roll, pitch, yaw):
+    def get_quaternion_from_euler(self, roll, pitch, yaw):
         qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
         qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
         qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
         qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
- 
         return [qx, qy, qz, qw]
 
     def set_odom(self):
@@ -83,21 +73,23 @@ class Calc_Odom:
         self.get_distR()
 
         #Velocities Calc. 
-        vl = self.R*(self.velRight+self.velLeft)/2
-        w = self.R*(self.velRight-self.velLeft)/(2*self.L)
+        vl = (self.velRight+self.velLeft)/2
+        w= (self.velRight-self.velLeft)/(2*self.L)
         
         #Pose Calc.
         distance_center = (self.distanceL + self.distanceR)/2
-        dtheta = (self.distanceL - self.distanceR)/self.L
+        dtheta = (self.distanceL - self.distanceR)/(2*self.L)
 
-        self.newTheta = self.oldTheta+dtheta
-        self.newX = self.oldX+distance_center*cos(self.oldTheta)
-        self.newY = self.oldY+distance_center*cos(self.oldTheta)
+        self.X = self.X+distance_center*cos(self.Theta)
+        self.Y = self.Y+distance_center*sin(self.Theta)
+        self.Theta = self.Theta+dtheta
         
-        [qx, qy, qz, qw] = self.get_quaternion_from_euler(0,0,self.newTheta)
+        
+        [qx, qy, qz, qw] = self.get_quaternion_from_euler(0,0,self.Theta)
+        print(180*dtheta/pi)
         now =rospy.get_rostime()
-        self.odom_msg.pose.pose.position.x = self.newX
-        self.odom_msg.pose.pose.position.y = self.newY
+        self.odom_msg.pose.pose.position.x = self.X
+        self.odom_msg.pose.pose.position.y = self.Y
         self.odom_msg.pose.pose.position.z = 0
         self.odom_msg.pose.pose.orientation.x = qx
         self.odom_msg.pose.pose.orientation.y = qy
@@ -118,7 +110,9 @@ if __name__ == '__main__':
         
         try:
             odom = Calc_Odom()
+            time.sleep(0.1)
             while not rospy.is_shutdown():
+                
                odom.set_odom() 
 
         except rospy.ROSInterruptException: pass
