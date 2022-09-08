e6fda3373a786227527cec12fe5b6260d2930d81
maarinaabatista <marina.batista@ee.ufcg.edu.br>
2022-09-08 17:41:24 -0300

    Teste - realsense, ros e pyrealsense

--- Devices/RealSense/src/rs_Depth.py
+++ Devices/RealSense/src/rs_Depth.py
@@ -1,76 +1,97 @@
-## License: Apache 2.0. See LICENSE file in root directory.
-## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
-
-###############################################
-##      Open CV and Numpy integration        ##
-###############################################
-
+from email import message_from_binary_file
+import queue
+from socket import MsgFlag
+from unicodedata import name
 import pyrealsense2 as rs
 import numpy as np
 import cv2
+import rospy
+from sensor_msgs.msg import Image , CameraInfo
+import time
 
-# Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
+from cv_bridge import CvBridge, CvBridgeError
 
-# Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-found_rgb = False
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-        break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)
+class Ros_Camera:
+    def __init__(self):
+        self.rospy=rospy
+        self.rospy.init_node('camera')
+        self.initPublishers()
+        
+        self.rate = rospy.Rate(10)
 
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+    def initPublishers(self):
+        self.image_publisher = rospy.Publisher('/camera/color/image_raw', Image, queue_size=10)
+        self.depth_publisher = rospy.Publisher('/camera/depth/image_raw', Image, queue_size=10)
+        self.color_camera_info = rospy.Publisher('/camera/color/camera_info', CameraInfo, queue_size = 10)
+        self.rate = rospy.Rate(10)
 
-if device_product_line == 'L500':
-    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-# Start streaming
-pipeline.start(config)
+if __name__ == '__main__':
 
-try:
-    while True:
+    
+    camera= Ros_Camera()
 
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
-            continue
+    width = 640
+    height = 480
 
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+    pipeline = rs.pipeline()
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, width, height, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, width, height, rs.format.bgr8, 30)
 
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+    profile = pipeline.start(config)
 
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
+    depth_sensor = profile.get_device().first_depth_sensor()
+    depth_scale = depth_sensor.get_depth_scale()
 
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap))
+    print("Depth Scale is: ", depth_scale)
 
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        cv2.waitKey(1)
+    try:
+        while not rospy.is_shutdown():
+            frames = pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            
+            
+            if not depth_frame or not color_frame:
+                continue
 
-finally:
+            # convert images to numpy arrays
+            depth_image = np.asanyarray(depth_frame.get_data())
+            color_image = np.asanyarray(color_frame.get_data())
 
-    # Stop streaming
-    pipeline.stop()
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+            
+            msg_frame_depth = CvBridge().cv2_to_imgmsg(depth_image, "16UC1")
+            msg_frame_depth.header.frame_id = 'camera_depth_link'
+            
+            msg_frame = CvBridge().cv2_to_imgmsg(color_image, "bgr8")
+            msg_frame.header.frame_id = 'camera_link'
+            
+            camera_info = CameraInfo()
+            
+            camera_info.height=480
+            camera_info.width= 640
+            camera_info.distortion_model= "plumb_bob"
+            camera_info.D= [0.0, 0.0, 0.0, 0.0, 0.0]
+            camera_info.K= [617.8533325195312, 0.0, 314.3265380859375, 0.0, 617.5131225585938, 241.8899688720703, 0.0, 0.0, 1.0]
+            camera_info.R= [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+            camera_info.P= [617.8533325195312, 0.0, 314.3265380859375, 0.0, 0.0, 617.5131225585938, 241.8899688720703, 0.0, 0.0, 0.0, 1.0, 0.0]
+        
+
+            camera.image_publisher.publish(msg_frame)
+            camera.depth_publisher.publish(msg_frame_depth)
+            camera.color_camera_info.publish(camera_info)
+
+            depth = depth_image[320,240].astype(float)*depth_scale
+
+            cv2.imshow('rgb', color_image)
+            cv2.imshow('depth', depth_colormap)
+            #print(f'Depth: {depth} m')
+
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+    finally:
+        pipeline.stop()
